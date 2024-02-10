db5f89fd1744f0410cb4b4350d3302948ccc245e
Mehrshad Lotfi Foroushani <mehrshad@mehrshad.io>
2024-02-10 22:39:06 +0100

    Add imprint to footer

--- src/app/imprint/page.tsx
+++ src/app/imprint/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { Mdx } from "@/app/components/mdx";
 import { Header } from "@/app/components/header";
 import { allPages } from "contentlayer/generated";
+import { Footer } from "@/app/components/footer";
 
 export const revalidate = 60;
 
@@ -15,11 +16,14 @@ export default async function PostPage() {
 	}
 
 	return (
+		<>
 		<div className="bg-zinc-50 min-h-screen">
 			<Header title={imprint.title}/>
 			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
 				<Mdx code={imprint.body.code} />
 			</article>
 		</div>
+		<Footer classNames="color-black-100 pt-4" />
+		</>
 	);
 }
